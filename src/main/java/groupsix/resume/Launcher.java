c6091f69568bdb74f194effb76877bd6f56523fd
agottschalk <a.gottschalk77@gmail.com>
2025-10-11 23:54:18 -0500

    added launcher

---
+++ src/main/java/groupsix/resume/Launcher.java
@@ -0,0 +1,13 @@
+package groupsix.resume;
+
+import javafx.application.Application;
+
+/**
+ * Alternate launcher class for starting the program.  Run from this file if running from
+ * ResumeMain.java doesn't work
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(ResumeMain.class, args);
+    }
+}
