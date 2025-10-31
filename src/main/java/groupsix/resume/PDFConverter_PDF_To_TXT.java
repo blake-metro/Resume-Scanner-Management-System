c905f14f70a6bf9c2bc47bd6366da20086b64d93
Johnson <carterevanjohnson@gmail.com>
2025-10-31 02:04:14 -0500

    Checking imports of PDFBox, Early PDF Converter. Happy Halloween!

---
+++ src/main/java/groupsix/resume/PDFConverter_PDF_To_TXT.java
@@ -0,0 +1,50 @@
+package groupsix.resume;
+
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+
+import java.io.File; //For file separator, if using
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+/*
+Class dedicated to taking in a PDF and outputting a plain text file
+ */
+
+
+public class PDFConverter_PDF_To_TXT {
+
+    public PDFConverter_PDF_To_TXT(){
+        ArrayList<String> pdfText = new ArrayList<String>();
+
+    }
+
+    public void convertPDF(){ //will convert all files that are in the
+
+    }
+
+
+    /*
+    Plan:
+    - UI will add uploaded PDFs to the src/ResumeStorage_PDFs folder, then will
+    use a for-each loop to grab and convert each pdf within that folder
+    - May not need to use "filePath" var then, if use for-each loop
+     */
+
+    //String filePath = null;
+    //filePath = "src" + File.separator + "ResumeStorage_PDFs" + File.separator + "FILE NAME, CHANGE THIS"; //Incomplete as of 10/31
+
+
+
+
+
+
+
+
+}
+
+
+
