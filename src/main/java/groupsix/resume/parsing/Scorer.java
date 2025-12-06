38c9878407298588fc3166e943c0f8cc34931c05
agottschalk <a.gottschalk77@gmail.com>
2025-12-05 23:22:38 -0600

    add Scorer utility class

---
+++ src/main/java/groupsix/resume/parsing/Scorer.java
@@ -0,0 +1,12 @@
+package groupsix.resume.parsing;
+
+import groupsix.resume.model.JobDescription;
+import groupsix.resume.model.Resume;
+
+public class Scorer {
+    private Scorer(){}
+
+    public static double calculateScore(Resume resume, JobDescription jobDescription) {
+        return 0;
+    }
+}
